6f27f4a13a3fee3b46c132733a7ef9981d60719e
Ahmed-S-Hamed <elidyan2010s@gmail.com>
2023-04-20 14:24:42 +0200

    ok

--- src/constants/dummy.js
+++ src/constants/dummy.js
@@ -4,27 +4,27 @@ export const DUMMY_SIMILAR = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     price: "9999 LE",
-    image: "/images/simi1.jpg",
+    image: "images/simi1.jpg",
   },
   {
     id: "P2",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     price: "9999 LE",
-    image: "/images/simi2.jpg",
+    image: "images/simi2.jpg",
   },
   {
     id: "P3",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     price: "9999 LE",
-    image: "/images/simi3.jpg",
+    image: "images/simi3.jpg",
   },
   {
     id: "P4",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     price: "9999 LE",
-    image: "/images/simi4.jpg",
+    image: "images/simi4.jpg",
   },
 ];
